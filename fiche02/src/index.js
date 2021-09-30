a8a72f60cafe493967f695aba4291bfca61fecae
soulaymanegharroudi <90785038+soulaymanegharroudi@users.noreply.github.com>
2021-09-30 14:05:58 +0200

    boilerplate and 2.2 done

---
+++ fiche02/src/index.js
@@ -0,0 +1,29 @@
+// Import Bootstrap CSS
+//import 'bootstrap/dist/css/bootstrap.min.css';
+// Custom styles
+//import './stylesheets/main.css';
+
+// This is the entry point to your app : add all relevant import and custom code
+import pizzaImg from "./img/pizza.jpg";
+import pizzaioloImg from "./img/pizzaiolo.jpg";
+
+const main = document.querySelector("main");
+const title = document.querySelector("title");
+const pizza = document.createElement("img");
+const pizzaiolo = document.createElement("img");
+const titre = document.createElement("p");
+
+titre.innerHTML="Bienvenu à la tour de Pise Ah!";
+
+title.innerHTML="La tour de Pise Ah!";
+
+pizza.src = pizzaImg;
+pizza.style=`width:100%;
+    height:500px;
+    `;
+
+pizzaiolo.src = pizzaioloImg;
+
+main.appendChild(titre);
+main.appendChild(pizza);
+main.appendChild(pizzaiolo);
