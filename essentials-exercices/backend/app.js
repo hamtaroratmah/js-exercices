0eb96458924190a32d259db1caaea7882b1112c0
soulaymanegharroudi <soulaymane.gharroudi@student.vinci.be>
2021-11-10 12:43:43 +0100

    api get fiche 04

--- essentials-exercices/backend/app.js
+++ essentials-exercices/backend/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var filmRouter = require('./routes/films');
 
 var app = express();
 
@@ -14,6 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/films', filmRouter);
 
 module.exports = app;
--- essentials-exercices/backend/routes/films.js
+++ essentials-exercices/backend/routes/films.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const { Films } = require("../models/films");
+const filmModel = new Films();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('film', { title: 'Express Film' });
+router.get('/', function(req, res) {
+    res.json(filmModel.getAll());
 });
+
+/* POST add films */
+router.post('/', function (req ,res ) {
+    if (
+        !req.body ||
+        !req.body.title ||
+        !req.body.link ||
+        !req.body.duration ||
+        !req.body.budget ||
+        !req.body.title ||
+        !req.body.title.trim() ||
+        !req.body.link.trim()
+    )
+        return res.sendStatus(400);
+
+    const film = filmModel.addOne(req.body);
+
+    return res.json(film);
+
+})
+
+router.get('/', function(req, res, next) {
+    if(!req.body.predicate) res.error(400);
+
+    return res.json(filmModel.getAll(req.body.predicate));
+});
+
 
 module.exports = router;
